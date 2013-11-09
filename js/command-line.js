c39f8bb3b41ea8aea7fe88bad2b6e60340245874
Lauri Raivio <lauri.raivio@gmail.com>
2013-11-09 19:43:37 +0200

    Added hogan.js for templating typeahead suggestions. Added info text to command suggestions.

--- js/command-line.js
+++ js/command-line.js
@@ -1,8 +1,30 @@
 $(document).ready(function() {
 
+  COMMANDS = [
+    {
+      value: 'sauna on',
+      info: 'Turns the sauna on'
+    },
+    {
+      value: 'sauna off',
+      info: 'Turns the sauna off'
+    },
+    {
+      value: 'lights on',
+      info: 'Turns the lights on'
+    },
+    {
+      value: 'lights off',
+      info: 'Turns the lights off'
+    }
+  ];
+
+
   $('#command-line').typeahead({
     name: 'commands',
-    local: ['sauna on', 'sauna off', 'lights on', 'lights off']
+    local: COMMANDS,
+    template: '<p><strong>{{value}}</strong> – <small>{{info}}</small></p>',
+    engine: Hogan
   });
 
 });
