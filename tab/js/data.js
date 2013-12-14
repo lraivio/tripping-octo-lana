7e39b41973b1937ae71c342a43b54d731eb1ef20
Tommi Hirvonen <tommi.hirvonen@aalto.fi>
2013-12-14 22:59:12 +0200

    Tab UI initial version

---
+++ tab/js/data.js
@@ -0,0 +1,48 @@
+﻿var pushupData = {
+    name: "Pushups (count)",
+    dropName: "P u s h u p s",
+    labels: true,
+    data: [
+        { date: '2013-06-14', value: 30 },
+        { date: '2013-06-17', value: 30 },
+        { date: '2013-06-19', value: 20 },
+        { date: '2013-06-20', value: 25 },
+        { date: '2013-06-23', value: 31 },
+        { date: '2013-06-28', value: 12 },
+        { date: '2013-06-30', value: 28 },
+        { date: '2013-07-05', value: 40 }
+    ]
+};
+
+var runningData = {
+    name: "Running (km)",
+    dropName: "R u n n i n g",
+    labels: true,
+    data: [
+        { date: '2013-06-15', value: 5 },
+        { date: '2013-06-17', value: 6 },
+        { date: '2013-06-20', value: 5 },
+        { date: '2013-06-25', value: 8 },
+        { date: '2013-06-30', value: 8 },
+        { date: '2013-07-04', value: 9 }
+    ]
+};
+
+var happinessData = {
+    name: "Happiness (smileys)",
+    dropName: "H a p p i n e s s",
+    labels: false,
+    data: [
+        { date: '2013-06-14', value: 1, marker: '/css/images/smile.png' },
+        { date: '2013-06-15', value: 0, marker: '/css/images/sad.png' },
+        { date: '2013-06-17', value: 1, marker: '/css/images/smile.png' },
+        { date: '2013-06-19', value: 1, marker: '/css/images/smile.png' },
+        { date: '2013-06-20', value: 0, marker: '/css/images/sad.png' },
+        { date: '2013-06-23', value: 1, marker: '/css/images/smile.png' },
+        { date: '2013-06-25', value: 1, marker: '/css/images/smile.png' },
+        { date: '2013-06-28', value: 0, marker: '/css/images/sad.png' },
+        { date: '2013-06-30', value: 1, marker: '/css/images/smile.png' },
+        { date: '2013-07-04', value: 0, marker: '/css/images/sad.png' },
+        { date: '2013-07-05', value: 0, marker: '/css/images/sad.png' }
+    ]
+};
---
+++ tab/js/lifelog.js
@@ -0,0 +1,129 @@
+﻿function ViewModel() {
+    var self = this;
+
+    self.yAxis1 = ko.observable(pushupData);
+    self.yAxis1.subscribe(function () { self.drawChart(); });
+    self.yAxis1Cumulative = ko.observable(false);
+    self.yAxis1Cumulative.subscribe(function() { self.drawChart(); });
+    self.yAxis1Name = ko.computed(function() {
+        return self.yAxis1() != null ? self.yAxis1().dropName : 'D r o p &nbsp; m e t r i c &nbsp; h e r e';
+    });
+
+    self.yAxis2 = ko.observable(null);
+    self.yAxis2.subscribe(function () { self.drawChart(); });
+    self.yAxis2Cumulative = ko.observable(false);
+    self.yAxis2Cumulative.subscribe(function () { self.drawChart(); });
+    self.yAxis2Name = ko.computed(function () {
+        return self.yAxis2() != null ? self.yAxis2().dropName : 'D r o p &nbsp; m e t r i c &nbsp; h e r e';
+    });
+
+    self.xAxis = ko.observable(null);
+    self.xAxis.subscribe(function () { self.drawChart(); });
+
+    self.chartHeight = ko.observable(0);
+
+    self.drawChart = function () {
+        var sum = 0;
+        var ya1Data = self.yAxis1() != null ? _.map(self.yAxis1().data, function (v) {
+            var marker = v.marker ? { symbol: 'url(' + v.marker + ')' } : null;
+            return { x: moment(v.date), y: self.yAxis1Cumulative() ? sum += v.value : v.value, marker: marker };
+        }) : null;
+        sum = 0;
+        var ya2Data = self.yAxis2() != null ? _.map(self.yAxis2().data, function (v) {
+            var marker = v.marker ? { symbol: 'url(' + v.marker + ')' } : null;;
+            return { x: moment(v.date), y: self.yAxis2Cumulative() ? sum += v.value : v.value, marker: marker };
+        }) : null;
+
+        var series = [];
+        if (self.yAxis1() != null)
+            series.push({
+                name: self.yAxis1().name + (self.yAxis1Cumulative() ? ' (cumulative)' : ''),
+                data: ya1Data,
+                yAxis: 0
+            });
+        if (self.yAxis2() != null)
+            series.push({
+                name: self.yAxis2().name + (self.yAxis2Cumulative() ? ' (cumulative)' : ''),
+                data: ya2Data,
+                yAxis: 1
+            });
+
+        self.chart = $('#chart').highcharts({
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: 'LifeLog',
+                x: -20
+            },
+            xAxis: {
+                type: self.xAxis() == null ? 'datetime' : '??',
+                title: {
+                    text: null
+                }
+            },
+            yAxis: [
+                {
+                    title: { text: self.yAxis1() != null ? self.yAxis1().name : '' }
+                },
+                {
+                    opposite: true,
+                    title: { text: self.yAxis2() != null ? self.yAxis2().name : ''}
+                }
+            ],
+            series: series
+        });
+        self.chartHeight($('#chart').height());
+    };
+
+    self.setDragDrop = function() {
+        $('.measures li').draggable({
+            helper: 'clone',
+            revert: 'invalid'
+        });
+        $('.ydrop1').droppable({
+            hoverClass: 'hover',
+            drop: function(e, u) {
+                self.setY1($(u.draggable).data('id'));
+            }
+        });
+        $('.ydrop2').droppable({
+            hoverClass: 'hover',
+            drop: function (e, u) {
+                self.setY2($(u.draggable).data('id'));
+            }
+        });
+    };
+
+    self.setY1 = function(id) {
+        if (id == 'pushups') {
+            self.yAxis1(pushupData);
+        } else if (id == 'running') {
+            self.yAxis1(runningData);
+        } else {
+            self.yAxis1(happinessData);
+        }
+    };
+
+    self.setY2 = function (id) {
+        if (id == 'pushups') {
+            self.yAxis2(pushupData);
+        } else if (id == 'running') {
+            self.yAxis2(runningData);
+        } else {
+            self.yAxis2(happinessData);
+        }
+    };
+
+    self.initialize = function () {
+        self.drawChart();
+        self.setDragDrop();
+    };
+}
+
+$(function() {
+    window.viewModel = new ViewModel();
+    ko.applyBindings(window.viewModel);
+
+    window.viewModel.initialize();
+});
